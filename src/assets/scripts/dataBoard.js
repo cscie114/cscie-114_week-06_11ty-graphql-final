374262708b327a8302e95cc181761663b56905aa
Michael D. Hilborn <mhilborn@biggles2k.com>
2023-02-28 11:16:42 -0500

    Initial draft of header, footer, and dashboard.

---
+++ src/assets/scripts/dataBoard.js
@@ -0,0 +1,23 @@
+window.addEventListener("DOMContentLoaded", function(e) {
+    console.log("DOM loaded...");
+
+    // Select all the anchor elements in the #dataBoard element and assign them
+    // an onClick event listener
+    let navListElements = document.querySelectorAll('div#dataBoard nav a');
+    
+    // For each anchor element, assign an onclick event listener that 
+    // causes its corresponding data list to appear.
+    navListElements.forEach(a => {
+        a.addEventListener('click', e => {
+            e.preventDefault();
+
+            // Cycle through all the dataList objects and set their css 
+            // 'display' property to 'none.' Set the anchor's
+            // corresponding dataList object 'display' to 'block.'
+            document.querySelectorAll('div.dataList').forEach(div => {
+                div.style.display = 'none';
+            });
+            document.querySelector(`#${a.text}`).style.display = 'block';
+        });
+    });
+});
